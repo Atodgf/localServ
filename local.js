40d60db1614c8cfb48eb00a8182810caf078b0dd
fgdota2 <68871109+fgdota2@users.noreply.github.com>
2020-12-04 11:56:36 +0300

    Create local.js

---
+++ local.js
@@ -0,0 +1,36 @@
+const http = require('http');
+const hostname = '127.0.0.1';
+const port = 3000;
+const users = ['Дима', 'Руслан', 'Катя', 'Сергей']
+
+
+const server = http.createServer((req, res) => {
+    if (req.method == 'GET'){
+        res.end(JSON.stringify(users))}
+
+    else if ((req.method == 'POST')){
+        let body = ''
+        req.on('data', (chunk => {
+            body+= chunk.toString()
+        }))
+        req.on('end', ()=> {
+            users.push(body)
+            res.end(JSON.stringify(users))
+        })
+    }
+    else if ((req.method == 'PUT')){
+        let body = ''
+        req.on('data', (chunk => {
+            body+= chunk.toString()
+        }))
+        req.on('end', ()=> {
+            users.splice(0, users.length, body)
+            res.end(JSON.stringify(users))
+        })
+    }
+
+});
+
+server.listen(port, hostname, () => {
+console.log(`Server running at http://${hostname}:${port}/`);
+});
